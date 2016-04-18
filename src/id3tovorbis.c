614e4a2c9896153f6c6267e0eb5e06879f9b76d8
Gaël Le Coz <github@le-coz.net>
2016-04-18 11:10:15 +0200

    - Added GNU GPL licence.

--- src/id3tovorbis.c
+++ src/id3tovorbis.c
@@ -1,12 +1,20 @@
-/*
- ============================================================================
- Name        : id3tovorbis.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+/*******************************************************************************
+ * Split - Split accounting amongst friends for travels or other group activities.
+ * Copyright (c) 2016 Gaël Le Coz.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
