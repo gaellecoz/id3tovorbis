d1270a5777e393f438b63fa6e7394e75a6fcf94d
Gaël Le Coz <github@le-coz.net>
2016-04-25 18:13:01 +0200

    - Added m4 folder.

--- myconfigure.sh
+++ myconfigure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LDFLAGS="$LDFLAGS -ggdb3 -L/Users/gaellecoz/git/id3tovorbis/libs/taglib-1.11beta2/taglib"
+export LDFLAGS="$LDFLAGS -ggdb3 -L/opt/local/lib -L/Users/gaellecoz/git/id3tovorbis/libs/taglib-1.11beta2/taglib"
 export CPPFLAGS="$CPPFLAGS -I/Users/gaellecoz/git/id3tovorbis/libs/taglib-1.11beta2 -I/Users/gaellecoz/git/id3tovorbis/libs/taglib-1.11beta2/taglib -I/Users/gaellecoz/git/id3tovorbis/libs/taglib-1.11beta2/taglib/toolkit"
 export CXXFLAGS="$CXXFLAGS -ggdb3 -O0"
 ./configure --enable-debug
